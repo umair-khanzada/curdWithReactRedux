aacb0bef2f8d6f604fe8514ebcdfad6659bf9c85
Umair Zulfiqar <umairkz52@gmail.com>
2017-02-13 20:23:28 +0500

    initailization

---
+++ src/reducers/data.js
@@ -0,0 +1,11 @@
+/**
+ * Created by appBakerz - 05 on 13-Feb-17.
+ */
+
+export default function(state = [], action){
+    switch (action.type) {
+        default:
+            return state
+    }
+
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by appBakerz - 05 on 13-Feb-17.
+ */
+
+
+import { combineReducers } from 'redux';
+import Data from './data';
+
+export default combineReducers({
+    Data
+});
